b5acf22df11580fc8d8302d44503bab6b2856eea
Rafal Cardona <rafael.cardona@alumnos.udg.mx>
2020-09-28 10:56:06 -0500

    Codigo para calcular area de un triangulo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    int a, b;
+    float area;
+
+    cout << "Ingresa la base del triangulo: ";
+    cin >> a;
+    cout << "Ingresa la altura del triangulo: ";
+    cin >> b;
+    area = float(a*b)/2;
+
+    cout << "El area del triangulo es "<<area;
+
+    getch();
 
     return 0;
 }
