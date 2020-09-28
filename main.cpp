9dac80d9b7e2b544e18de98a94181478aa859639
Rafal Cardona <rafael.cardona@alumnos.udg.mx>
2020-09-28 10:58:26 -0500

    Eliminacion de libreria conio.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
@@ -15,7 +14,6 @@ int main() {
 
     cout << "El area del triangulo es "<<area;
 
-    getch();
 
     return 0;
 }
